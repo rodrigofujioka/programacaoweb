59a912d7b61167c5a68eeb7e25454d17633adf77
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2019-05-25 00:16:53 -0300

    Aula do professor Rodrigo Fujioka

---
+++ src/test/java/com/rodrigofujioka/web/ProgramacaoAvancadaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigofujioka.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProgramacaoAvancadaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
